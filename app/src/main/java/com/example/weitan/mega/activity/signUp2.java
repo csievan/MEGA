7b9f54cc384ffca74ef194f47f6c44c842f6dbec
csievan <wuweitan5@gmail.com>
2017-08-31 15:31:15 +0700

    initial commit

---
+++ app/src/main/java/com/example/weitan/mega/activity/signUp2.java
@@ -0,0 +1,13 @@
+package com.example.weitan.mega.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signUp2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up2);
+    }
+}
